59b23bced227028dffe7e72f61a0e41050a57d60
VerificaZap <aumixpetshop@gmail.com>
2025-07-16 17:52:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,7 @@ def consulta_receita():
         return jsonify({"erro": "Erro ao consultar ReceitaWS", "detalhe": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=10000)
+
+
    
